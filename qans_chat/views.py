1a835af73489fbf1f2d702a5e5b34ed3c220000f
Rajesh P <rajesh.pethe@gmail.com>
2023-08-19 16:32:33 +0530

    The index view.

--- qans_chat/views.py
+++ qans_chat/views.py
@@ -1,3 +1,14 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    """
+    Returns JSON as response to a user query.
+    """
+    if request.method == "POST":
+        query = request.data.get("query")
+        # TODO: initialize results
+        result = {}
+        return JsonResponse(result)
+    return render(request, "base/index.html")
