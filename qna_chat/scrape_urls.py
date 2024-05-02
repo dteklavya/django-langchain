13af87ce6222f0be9f773517a3a365972bb13b35
Rajesh P <rajesh.pethe@gmail.com>
2024-05-02 19:08:18 +0530

    Add request headers.

--- qna_chat/scrape_urls.py
+++ qna_chat/scrape_urls.py
@@ -8,11 +8,19 @@ from bs4 import BeautifulSoup
 from pathlib import Path
 from urllib.parse import urlparse
 import requests
+from celery import shared_task
 
 
 def get_drf_urls() -> list:
     base_url = "https://www.django-rest-framework.org/"
-    base_response = requests.get(base_url)
+    base_response = requests.get(
+        base_url,
+        headers={
+            "Accept": "application/xml; charset=utf-8",
+            "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:123.0) Gecko/20100101 Firefox/123.0",
+        },
+        timeout=5,
+    )
     base_html = base_response.content.decode("utf-8")
     soup = BeautifulSoup(base_html, "html.parser")
 
@@ -38,6 +46,9 @@ def get_drf_urls() -> list:
         if path.endswith("announcement/") or path.startswith("#"):
             # Avoid version announcements
             continue
+
+        # if "tutorial" not in path:
+        #     continue
 
         if not path.startswith("#"):
             path = urlparse(path).path
